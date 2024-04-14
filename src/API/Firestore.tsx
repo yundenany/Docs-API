8114e554904743ce4b859cf104fdbbb728fca210
James Matthew <yundenany@gmail.com>
2024-04-15 00:36:07 +0700

    thirdcommit

--- src/API/Firestore.tsx
+++ src/API/Firestore.tsx
@@ -8,6 +8,9 @@ import {
   getDocs,
   getDoc,
 } from "firebase/firestore";
+import mammoth from "mammoth";
+
+//import firebase from "firebase/app";
 
 const docs = collection(firestore, "docs");
 
@@ -56,3 +59,54 @@ export const getCurrentDoc = (id: string, setCurrentDocument: any) => {
       console.log(err);
     });
 };
+
+export const handleFileChange = async (
+  event: React.ChangeEvent<HTMLInputElement>,
+  setValue: React.Dispatch<React.SetStateAction<string>>
+) => {
+  const file = event.target.files?.[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = async (event) => {
+      if (event.target) {
+        const result = event.target.result as ArrayBuffer;
+        // Chuyển đổi tệp .docx thành HTML bằng mammoth
+        const { value } = await mammoth.convertToHtml({ arrayBuffer: result });
+        // Lưu nội dung HTML vào Firestore
+        //await addDoc(collection(firestore, 'docs'), { content: result });
+        await addDoc(collection(firestore, "docs"), { value });
+        // Cập nhật state của nội dung để hiển thị trên trang
+        setValue(value.substring(0, 100));
+      }
+    };
+    reader.readAsArrayBuffer(file);
+  }
+};
+
+// export const handleFileChange = async (
+//   event: React.ChangeEvent<HTMLInputElement>,
+//   setValue: React.Dispatch<React.SetStateAction<string>>
+// ) => {
+//   const file = event.target.files?.[0];
+//   if (file) {
+//     const reader = new FileReader();
+//     reader.onload = async (event) => {
+//       if (event.target) {
+//         const result = event.target.result as ArrayBuffer;
+//         const { value } = await mammoth.convertToHtml({ arrayBuffer: result });
+
+//         // Phân tích HTML và lấy ra văn bản
+//         const parser = new DOMParser();
+//         const htmlDoc = parser.parseFromString(value, "text/html");
+//         const text = htmlDoc.body.textContent || "";
+
+//         // Lưu văn bản vào Firestore
+//         await addDoc(collection(firestore, "docs"), { value });
+
+//         // Cập nhật state của nội dung để hiển thị trên trang
+//         setValue(text.substring(0, 100));
+//       }
+//     };
+//     reader.readAsArrayBuffer(file);
+//   }
+// };
