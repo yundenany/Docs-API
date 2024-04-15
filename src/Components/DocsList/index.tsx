698a0c78e7471f12197667e7903bb947205f9afd
James Matthew <yundenany@gmail.com>
2024-04-15 13:17:15 +0700

    yes

--- src/Components/DocsList/index.tsx
+++ src/Components/DocsList/index.tsx
@@ -2,11 +2,7 @@ import "./index.scss";
 import { getDocuments } from "../../API/Firestore";
 //import FileSaver from "file-saver"; // Import thư viện file-saver
 import { useEffect, useState, useCallback } from "react";
-// import {
-//   DocumentEditorContainerComponent,
-//   Toolbar,
-//   Inject,
-// } from "@syncfusion/ej2-react-documenteditor";
+
 type OpenDocType = {
   openDoc: (id: string, value: string, title: string) => void;
 };
